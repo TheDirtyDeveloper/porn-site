64f3909ba2c458adbc3befe6c620145c3451a466
Cospel1 <liottafabrizio@gmail.com>
2018-06-12 19:36:02 -0300

    Prettier tags

--- public/js/helper.js
+++ public/js/helper.js
@@ -2,3 +2,17 @@ $("#search_b").click(function(){
     var search = $("#search").val();
     window.location.href = `http://localhost:3000/1/${search}`;
 }); 
+
+$("#expander").click(function(){
+    const tag_size = $("#taglist").height();
+    const tagdiv_size = $("#tags").height();
+    
+    if(tagdiv_size == 50){
+        $("#tags").css("height",tag_size);
+        $("#expander").text("(Hide)");
+    }else{
+        $("#tags").css("height","50");
+        $("#expander").text("(Expand)");
+    }
+    
+});
