3ab0cdabb447fec141a4620d76ce8754b9374647
Philip Bankier <pbankier@miso3.com>
2018-11-05 21:43:22 -0500

    fixes and work on categories page

--- server/routes/tag.js
+++ server/routes/tag.js
@@ -20,4 +20,20 @@ router.get('/:tag', (req,res) => {
          .catch((e) => res.status(400).send(e));
 });
 
+router.get('/', (req,res) => {
+    var page = parseInt(req.query.p);
+    if(!page || page == 0) page = 1;
+
+    const next = `tags?p=${page+1}`;
+    const back = `tags?p=${page-1}`;
+    axios.get(`https://api.redtube.com/?data=redtube.Stars.getStarDetailedList&output=json&page=${page}`)
+        .then((stars) => {
+            if(stars.data.count == 0)
+                return res.render('error', {error_code: 404, error_message: "Oopsie, there are no pornstars"});
+            const pornstars = stars.data.stars;
+            res.status(200).render('tags', {pornstars: pornstars, next: next, back: back});
+        })
+        .catch((e) => res.status(400).send(e));
+});
+
 module.exports = router;
